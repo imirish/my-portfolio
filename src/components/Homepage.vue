bcfd474b81af8512c0f29062538dea805f90ac17
Danica Irish Garcia <62048904+imirish@users.noreply.github.com>
2025-08-17 17:42:22 +0800

    Update

---
+++ src/components/Homepage.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Hey There!</h1>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+
+export default {
+  props: {
+    msg: {
+      type: String,
+      default: "",
+    },
+  },
+  setup() {
+    const count = ref(0);
+
+    return {
+      count,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
