553d522b4689737304d146eea56d6ee16ff81480
Danica Irish Garcia <62048904+imirish@users.noreply.github.com>
2025-08-17 17:08:29 +0800

    Init

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: "#app",
+  prefix: "tw-",
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
