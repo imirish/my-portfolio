a17d75ce85391db69dd3671cb4e44c07157a9d46
Danica Irish Garcia <62048904+imirish@users.noreply.github.com>
2025-09-01 22:23:04 +0800

    Update route

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/my-profile/",
+  base: "/my-profile-danica/",
   plugins: [vue()],
   resolve: {
     alias: {
