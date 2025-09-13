fdfbb820a5a28f93f067f99dac0f07416b8e5210
Danica Irish Garcia <62048904+imirish@users.noreply.github.com>
2025-09-13 16:59:24 +0800

    Update design

--- src/components/common/NavigationV2.vue
+++ src/components/common/NavigationV2.vue
@@ -2,7 +2,7 @@
   <div class="sticky top-0 z-20">
     <div class="relative flex justify-end">
       <div
-        class="absolute flex flex-row items-end px-6 xs:px-10 justify-end rounded-full py-3 text-12px xs:text-16px"
+        class="absolute flex flex-row xs:gap-1 md:gap-4 items-end px-6 xs:px-10 justify-end rounded-full py-3 text-12px xs:text-16px"
       >
         <div
           v-for="(path, key) in paths"
